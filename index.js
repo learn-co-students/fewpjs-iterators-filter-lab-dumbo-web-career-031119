3aea55fbb82b38ca232df824ad31e1fcc32b514a
tpr1992 <terence.pataneronan@gmail.com>
2019-04-23 18:10:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+
+
+const findMatching = (arr, string) => {
+  return arr.filter(driver => {
+    return driver.toLowerCase() === string.toLowerCase()
+  })
+}
+
+const fuzzyMatch = (arr, string) => {
+  let length = string.length
+  return arr.filter(driver => {
+    return driver.slice(0, length) === string;
+  })
+}
+
+const matchName = (arr, string) => {
+  return arr.filter(driver => {
+    return driver.name.toLowerCase() === string.toLowerCase();
+  })
+}
+
+
+
+
+
+
+
+// function findMatching(arr, string) {
+//   return arr.filter(function(driver) {
+//     return driver.toLowerCase() === string.toLowerCase();
+//   }) // => returns "hi"
+// }
+
+// findMatching(["bye", "hi"], "hi")
